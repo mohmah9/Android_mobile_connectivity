38194c207c9fc94426110c89a859b089c3d225df
mahdi <Mohamad Mahdi Yadegar>
2020-06-11 14:23:16 +0430

    Use dexter for permission granting

--- app/src/main/java/com/iust/rhodium_android/MainActivity.kt
+++ app/src/main/java/com/iust/rhodium_android/MainActivity.kt
@@ -1,11 +1,51 @@
 package com.iust.rhodium_android
 
+import android.Manifest
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.telephony.TelephonyManager
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.MultiplePermissionsReport
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener
+
 
 class MainActivity : AppCompatActivity() {
+
+    private var permit_s =1
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        permissiongranter()
+    }
+    private fun permissiongranter() {
+        Dexter.withContext(this)
+            .withPermissions(
+                Manifest.permission.READ_PHONE_STATE,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            )
+            .withListener(object : MultiplePermissionsListener {
+                override fun onPermissionsChecked(p0: MultiplePermissionsReport?) {
+                    Toast.makeText(this@MainActivity, "permission granted", Toast.LENGTH_SHORT)
+                        .show()
+                }
+
+                override fun onPermissionRationaleShouldBeShown(
+                    p0: MutableList<com.karumi.dexter.listener.PermissionRequest>?,
+                    p1: PermissionToken?
+                ) {
+                    ActivityCompat.requestPermissions(
+                        this@MainActivity, arrayOf(
+                            Manifest.permission.READ_PHONE_STATE,
+                            Manifest.permission.ACCESS_FINE_LOCATION
+                        ), permit_s
+                    )
+
+
+                }
+
+            }).check()
     }
 }
